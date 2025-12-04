5ba1a5d26d14b3b081d4ec38225d8a65abeffb98
Andrea Oggioni <24810621+etabeta1@users.noreply.github.com>
2025-12-04 18:03:18 +0100

    feat(point): implement elementwise sum

--- src/point.hpp
+++ src/point.hpp
@@ -2,30 +2,31 @@
 #define _POINT_HPP_
 
 #include <vector>
+#include <functional>
 
 namespace Swarm
 {
-    template<typename T, typename U>
+    template <typename T, typename U>
     concept Addable = requires(T a, U b) {
         a + b;
     };
 
-    template<typename T, typename U>
+    template <typename T, typename U>
     concept Subtractable = requires(T a, U b) {
         a - b;
     };
 
-    template<typename T, typename U>
+    template <typename T, typename U>
     concept Multipliable = requires(T a, U b) {
         a * b;
     };
 
-    template<typename T, typename U>
+    template <typename T, typename U>
     concept Divisible = requires(T a, U b) {
         a / b;
     };
 
-    template<typename T, typename U, typename V>
+    template <typename T, typename U, typename V>
     concept TriComparable = requires(T a, U b, V c) {
         a <= b <= c;
     };
@@ -34,62 +35,98 @@ namespace Swarm
     class Point
     {
     public:
-        Point(std::vector<T>& data) {
-            for(size_t i = 0; i < dim; i++) {
+        Point(std::vector<T> &data)
+        {
+            for (size_t i = 0; i < dim; i++)
+            {
                 coordinates[i] = data[i];
             }
         }
 
-        Point(T data[dim]) {
-            for(size_t i = 0; i < dim; i++) {
+        Point(T data[dim])
+        {
+            for (size_t i = 0; i < dim; i++)
+            {
                 coordinates[i] = data[i];
             }
         }
 
-        Point(T data) {
-            for(size_t i = 0; i < dim; i++) {
+        Point(T data)
+        {
+            for (size_t i = 0; i < dim; i++)
+            {
                 coordinates[i] = data;
             }
         }
 
-        Point(Point<T, dim>& data) {
-            for(size_t i = 0; i < dim; i++) {
+        Point(const Point<T, dim> &data)
+        {
+            for (size_t i = 0; i < dim; i++)
+            {
                 coordinates[i] = data.coordinates[i];
             }
         }
 
-        Point(std::function<T(size_t)> generator) {
-            for(size_t i = 0; i < dim; i++) {
+        Point(const std::function<T(size_t)>& generator)
+        {
+            for (size_t i = 0; i < dim; i++)
+            {
                 coordinates[i] = generator(i);
             }
         }
 
-        template<typename U> requires Addable<T, U>
-        Point<T, dim> operator+(Point<U, dim>& other) const {}
+        template <typename U>
+            requires Addable<T, U>
+        Point<T, dim> operator+(const Point<U, dim> &other) const
+        {
+            return Point<T, dim>([this, &other](size_t index) {
+                return coordinates[index] + other.coordinates[index];
+            });
+        }
 
-        template<typename U> requires Subtractable<T, U>
-        Point<T, dim> operator-(Point<U, dim>& other) const {}
+        template <typename U>
+            requires Subtractable<T, U>
+        Point<T, dim> operator-(Point<U, dim> &other) const
+        {
+        }
 
-        template<typename U> requires Multipliable<T, U>
-        Point<T, dim> operator*(U other) const {}
+        template <typename U>
+            requires Multipliable<T, U>
+        Point<T, dim> operator*(U other) const
+        {
+        }
 
-        template<typename U> requires Multipliable<T, U>
-        Point<T, dim> operator*(Point<U, dim>& other) const {}
+        template <typename U>
+            requires Multipliable<T, U>
+        Point<T, dim> operator*(Point<U, dim> &other) const
+        {
+        }
 
-        template<typename U> requires Divisible<T, U>
-        Point<T, dim> operator/(Point<U, dim>& other) const {}
+        template <typename U>
+            requires Divisible<T, U>
+        Point<T, dim> operator/(Point<U, dim> &other) const
+        {
+        }
 
-        template<typename U> requires Divisible<T, U>
-        Point<T, dim> operator/(U other) const {}
+        template <typename U>
+            requires Divisible<T, U>
+        Point<T, dim> operator/(U other) const
+        {
+        }
 
-        T& operator[](size_t index) const {}
+        T &operator[](size_t index) const {}
 
-        template<typename U, typename V> requires TriComparable<U, T, V>
-        Point<T, dim> clamp(Point<U, dim>& a, Point<V, dim>& b) {}
-    
+        template <typename U, typename V>
+            requires TriComparable<U, T, V>
+        Point<T, dim> clamp(Point<U, dim> &a, Point<V, dim> &b)
+        {
+        }
+
     private:
         T coordinates[dim];
     };
 };
+
+
 
 #endif
