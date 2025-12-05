27317603698f130c8db09b09a90c8dc5dbf9a188
Paolo Salvi <158834335+paull194@users.noreply.github.com>
2025-12-05 12:07:40 +0100

    Added Alpine1Function, AckleyFunction classes

--- src/swarm.hpp
+++ src/swarm.hpp
@@ -72,7 +72,40 @@ namespace Swarm
         }
     };
 
+    template <typename T = float, int dim = 2>
+    class Alpine1Function : public Function<T, dim>
+    {
+    public:
+        float evaluate(const Point<T, dim>& p) const override {
+            float sum = 0.0f;
+
+            for (int i = 0; i < dim; ++i) {
+                sum += abs(p[i] * sin(p[i]) + 0.1 * p[i]);
+            }
+            return sum;
+        }
+    };
+
+    template <typename T = float, int dim = 2>
+    class AckleyFunction : public Function<T, dim>
+    {
+    public:
+        float evaluate(const Point<T, dim>& p) const override {
+            float sum = 0.0f;
+            float part_sum_1 = 0.0f;
+            float part_sum_2 = 0.0f;
+            for (int i = 0; i < dim; ++i) {
+               part_sum_1 += p[i]*p[i];
+                part_sum_2 += cos(2 * M_PI * p[i]); 
+            }
+            sum = -20 * exp(-0.2 * sqrt(part_sum_1 / dim)) - exp(part_sum_2 / dim) + 20 + exp(1);
+            return sum;
+        }
+    };
+
     
+
+
 
 
 
