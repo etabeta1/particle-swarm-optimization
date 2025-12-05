a82ccadbc8785c266f331798c73cc6e9810cdf90
dmitriimeshcheriakov <dmitrii.meshcheriakov@mail.polimi.it>
2025-12-05 11:36:37 +0000

    feat:test chaopsMap

--- src/chaosmap.hpp
+++ src/chaosmap.hpp
@@ -9,7 +9,7 @@ namespace Swarm
 
 {
     template <typename T = float, int dim = 2>
-    using MapFunction = std::function<Point<T, dim>(Point<T, dim> &, int)>;
+    using MapFunction = std::function<Point<T, dim>(const Point<T, dim> &, int)>;
 
     template <typename T = float, int dim = 2>
     class ChaosMap
@@ -18,7 +18,7 @@ namespace Swarm
         Point<T, dim> a_min;
         Point<T, dim> b_max;
 
-        MapFunction map;
+        MapFunction<T,dim> map;
         inline Point<T, dim> mapBetweenDomains(const Point<T, dim> &point, const Point<T, dim> &first_range_start, const Point<T, dim> &first_range_end, const Point<T, dim> &min_range, const Point<T, dim> &max_range)
         {
             // codice per trasformare le coordinate
@@ -31,7 +31,7 @@ namespace Swarm
         };
 
     public:
-        ChaosMap(const MapFunction &lambda_fun, const Point<T, dim> &min, const Point<T, dim> &max) : map(lambda_fun), a_min(min), b_max(max) {};
+        ChaosMap(const MapFunction<T,dim> &lambda_fun, const Point<T, dim> &min, const Point<T, dim> &max) : map(lambda_fun), a_min(min), b_max(max) {};
         inline Point<T, dim> toLocalDomain(const Point<T, dim> &point, const Point<T, dim> &min_range, const Point<T, dim> &max_range)
         {
             return mapBetweenDomains(point, min_range, max_range, a_min, b_max);
@@ -42,9 +42,7 @@ namespace Swarm
         };
         inline Point<T, dim> getPoint(const Point<T, dim> &point, const Point<T, dim> &min_global, const Point<T, dim> &max_global, int iter)
         {
-            Point<T, dim> new_point;
-
-            new_point = toLocalDomain(point, min_global, max_global);
+            Point<T, dim> new_point = toLocalDomain(point, min_global, max_global);
             new_point = generatePoint(new_point, iter);
             new_point = toGlobalDomain(new_point, min_global, max_global);
 
