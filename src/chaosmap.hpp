2f26bf044d78112406d4dd3b166dd85a6b28e4d4
dmitriimeshcheriakov <dmitrii.meshcheriakov@mail.polimi.it>
2025-12-05 11:17:38 +0000

    feat:implement MapFunction typedef

--- src/chaosmap.hpp
+++ src/chaosmap.hpp
@@ -6,27 +6,32 @@
 #include <functional>
 
 namespace Swarm
+
 {
+    template <typename T = float, int dim = 2>
+    using MapFunction = std::function<Point<T, dim>(Point<T, dim> &, int)>;
+
     template <typename T = float, int dim = 2>
     class ChaosMap
     {
     private:
         Point<T, dim> a_min;
         Point<T, dim> b_max;
-        std::function<Point<T, dim>(Point<T, dim> &)> map;
+
+        MapFunction map;
         inline Point<T, dim> mapBetweenDomains(const Point<T, dim> &point, const Point<T, dim> &first_range_start, const Point<T, dim> &first_range_end, const Point<T, dim> &min_range, const Point<T, dim> &max_range)
         {
             // codice per trasformare le coordinate
             return (point - first_range_start) / (first_range_end - first_range_start) * (max_range - min_range) + min_range;
         };
-        inline Point<T, dim> generatePoint(const Point<T, dim> &point)
+        inline Point<T, dim> generatePoint(const Point<T, dim> &point, int iter)
         {
             // map evaluate point map(Point),per ogni dimensione genera per ogni coordinata
-            return map(point);
+            return map(point, iter);
         };
 
     public:
-        ChaosMap(const std::function<Point<T, dim>(Point<T, dim> &)> &lambda_fun, const Point<T, dim> &min, const Point<T, dim> &max) : map(lambda_fun), a_min(min), b_max(max) {};
+        ChaosMap(const MapFunction &lambda_fun, const Point<T, dim> &min, const Point<T, dim> &max) : map(lambda_fun), a_min(min), b_max(max) {};
         inline Point<T, dim> toLocalDomain(const Point<T, dim> &point, const Point<T, dim> &min_range, const Point<T, dim> &max_range)
         {
             return mapBetweenDomains(point, min_range, max_range, a_min, b_max);
@@ -35,12 +40,12 @@ namespace Swarm
         {
             return mapBetweenDomains(point, a_min, b_max, min_range, max_range);
         };
-        inline Point<T, dim> getPoint(const Point<T, dim> &point, const Point<T, dim> &min_global, const Point<T, dim> &max_global)
+        inline Point<T, dim> getPoint(const Point<T, dim> &point, const Point<T, dim> &min_global, const Point<T, dim> &max_global, int iter)
         {
             Point<T, dim> new_point;
 
             new_point = toLocalDomain(point, min_global, max_global);
-            new_point = generatePoint(new_point);
+            new_point = generatePoint(new_point, iter);
             new_point = toGlobalDomain(new_point, min_global, max_global);
 
             return new_point;
