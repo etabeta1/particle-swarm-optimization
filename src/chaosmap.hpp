30282fb4a654d73382d4f232e93478c30a240ad2
dmitriimeshcheriakov <dmitrii.meshcheriakov@mail.polimi.it>
2025-12-09 08:59:03 +0000

    feat:comment on ChaosMap

--- src/chaosmap.hpp
+++ src/chaosmap.hpp
@@ -4,7 +4,7 @@
 #include "point.hpp"
 
 #include <functional>
-
+//Chaosmap is used to generate Random coordinates for Chaotic Particles.
 namespace Swarm
 
 {
@@ -19,11 +19,13 @@ namespace Swarm
         Point<T, dim> b_max;
 
         MapFunction<T,dim> map;
+        //Esegue la trasformazione delle coordiante del punto in un'altro dominio
         inline Point<T, dim> mapBetweenDomains(const Point<T, dim> &point, const Point<T, dim> &first_range_start, const Point<T, dim> &first_range_end, const Point<T, dim> &min_range, const Point<T, dim> &max_range)
         {
             // codice per trasformare le coordinate
             return (point - first_range_start) / (first_range_end - first_range_start) * (max_range - min_range) + min_range;
         };
+        //genera un punto secondo la lambda function
         inline Point<T, dim> generatePoint(const Point<T, dim> &point, int iter)
         {
             // map evaluate point map(Point),per ogni dimensione genera per ogni coordinata
@@ -31,15 +33,19 @@ namespace Swarm
         };
 
     public:
+        // ChaosMap Construct
         ChaosMap(const MapFunction<T,dim> &lambda_fun, const Point<T, dim> &min, const Point<T, dim> &max) : map(lambda_fun), a_min(min), b_max(max) {};
+        //change coordinates of the Point from Global domain to local domain
         inline Point<T, dim> toLocalDomain(const Point<T, dim> &point, const Point<T, dim> &min_range, const Point<T, dim> &max_range)
         {
             return mapBetweenDomains(point, min_range, max_range, a_min, b_max);
         };
+        //change coordinates of the Point from Local domain to Global domain
         inline Point<T, dim> toGlobalDomain(const Point<T, dim> &point, const Point<T, dim> &min_range, const Point<T, dim> &max_range)
         {
             return mapBetweenDomains(point, a_min, b_max, min_range, max_range);
         };
+        //return a New Point from ChaosFunction
         inline Point<T, dim> getPoint(const Point<T, dim> &point, const Point<T, dim> &min_global, const Point<T, dim> &max_global, int iter)
         {
             Point<T, dim> new_point = toLocalDomain(point, min_global, max_global);
