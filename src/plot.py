856e59392541f247be65527ac38e182f6b90ad58
dmitriimeshcheriakov <dmitrii.meshcheriakov@mail.polimi.it>
2025-12-11 10:47:58 +0000

    feat: Implement Visualization for PSO

--- src/plot.py
+++ src/plot.py
@@ -1,32 +1,71 @@
 import numpy as np
+import matplotlib
+
+#matplotlib.use('MacOSX')
+#matplotlib.use('TkAgg') for Windows
+
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
-# --- Make input grid for the surface ---
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
-X, Y = np.meshgrid(x, y)
+data = np.loadtxt("points_xy.txt")
 
-# Define a function z = f(x, y)
-Z = np.sin(np.sqrt(X**2 + Y**2))
+x = data[:, 0]
+y = data[:, 1]
+z = data[:, 2]
+k = data[:, 3]
+unique_k = np.unique(k)
 
-# --- Create some points to overlay ---
-# Example: random (x, y) in the same range, with z on the surface
-x_points = np.random.uniform(-5, 5, 100)
-y_points = np.random.uniform(-5, 5, 100)
-z_points = np.sin(np.sqrt(x_points**2 + y_points**2))
 
-# --- Plot everything on the SAME axes ---
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection="3d")
 
-# Surface
-ax.plot_surface(X, Y, Z, alpha=0.7, cmap='viridis')  # alpha to see points better
+xmin, xmax = np.min(x), np.max(x)
+ymin, ymax = np.min(y), np.max(y)
+zmin, zmax = np.min(z), np.max(z)
 
-# Points
-ax.scatter(x_points, y_points, z_points, s=20)        # you can tweak s for size
 
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-ax.set_zlabel("z")
+ax.set_xlim(xmin, xmax)
+ax.set_ylim(ymin, ymax)
+ax.set_zlim(zmin - 1, zmax + 1)  
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+
+X_grid = np.linspace(xmin, xmax, 100)
+Y_grid = np.linspace(ymin, ymax, 100)
+X_surf, Y_surf = np.meshgrid(X_grid, Y_grid)
+
+Z_surf = np.sin(np.sqrt(X_surf ** 2 + Y_surf ** 2))
+
+ax.plot_surface(X_surf, Y_surf, Z_surf, cmap='coolwarm', alpha=0.3)
+
+k0 = unique_k[0]
+title_obj = ax.set_title(f"k = {k0}")
+scat = ax.scatter([], [], [], c=[])  
+
+
+def update(k_value):
+    global scat
+
+    if scat:
+        scat.remove()
+
+    mask = np.isclose(k, k_value)
+
+    scat = ax.scatter(x[mask], y[mask], z[mask], c=z[mask], cmap="viridis", s=40, depthshade=False)
+
+    title_obj.set_text(f"k = {k_value}")
+
+    return []
+
+ani = FuncAnimation(
+    fig,
+    update,
+    frames=unique_k,
+    interval=500,
+    repeat=True,
+    blit=False
+)
 
 plt.show()
