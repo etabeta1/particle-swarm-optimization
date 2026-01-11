c4222e6856ccccaa851b65271dc89cd9ad3cd743
Andrea Oggioni <24810621+etabeta1@users.noreply.github.com>
2026-01-11 13:59:11 +0100

    chore(docs): improve documentation

--- src/point.hpp
+++ src/point.hpp
@@ -10,17 +10,24 @@
 
 namespace Swarm
 {
-    // Representation of an n-dimensional vector.
-    //
-    // - `T` is the type used to store the coordinated (defaults to `float`).
-    // - `dim` is the number of dimensions for the vector (defaults to 2).
+    /**
+     * \brief Class representing an n-dimensional vector.
+     * \tparam T The data type for the coordinates (default is float).
+     * \tparam dim The dimensionality of the point (default is 2).
+     * \see Addable
+     * \see Multipliable
+     */
     template <typename T = float, int dim = 2>
         requires Addable<T, T> ||
                  Multipliable<T, T>
     class Point
     {
     public:
-        // Builds a `Point` using the first `dim` elements of an `std::vector`.
+        /**
+         * \brief Builds a `Point` using the elements from a vector.
+         * \param data The vector containing the coordinate values.
+         * \see std::vector
+         */
         Point(std::vector<T> &data)
         {
             for (size_t i = 0; i < dim; i++)
@@ -29,7 +36,10 @@ namespace Swarm
             }
         }
 
-        // Builds a `Point` using the elements from an array.
+        /**
+         * \brief Builds a `Point` using the elements from a raw array.
+         * \param data The array containing the coordinate values.
+         */
         Point(T data[dim])
         {
             for (size_t i = 0; i < dim; i++)
@@ -38,7 +48,10 @@ namespace Swarm
             }
         }
 
-        // Builds a `Point` where all the members are set to the same constant.
+        /**
+         * \brief Builds a `Point` with all coordinates set to the same value.
+         * \param data The value to set for all coordinates.
+         */
         Point(T data)
         {
             for (size_t i = 0; i < dim; i++)
@@ -47,7 +60,10 @@ namespace Swarm
             }
         }
 
-        // Copy constructor
+        /**
+         * \brief Builds a `Point` as a copy of another `Point`.
+         * \param data The `Point` to copy.
+         */
         Point(const Point<T, dim> &data)
         {
             for (size_t i = 0; i < dim; i++)
@@ -56,9 +72,13 @@ namespace Swarm
             }
         }
 
-        // Builds a `Point` using the returned values from a given lambda.
-        //
-        // The lambda is called `dim` times, with an incremental index as paramenter that starts at 0 and then it is incremented by one after each iteration.
+        /**
+         * \brief Builds a `Point` using a generator function.
+         * \param generator A function that generates the coordinate values based on their index.
+         * \see std::function
+         *
+         * The lambda is called `dim` times, with an incremental index as paramenter that starts at 0 and then it is incremented by one after each iteration.
+         */
         Point(const std::function<T(size_t)> &generator)
         {
             for (size_t i = 0; i < dim; i++)
@@ -67,7 +87,12 @@ namespace Swarm
             }
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise sum of the coordinates of two `Point`s.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise addition of the coordinates of two `Point`s.
+         * \param other The other `Point` to add.
+         * \return A new `Point` representing the elementwise sum.
+         * \see Addable
+         */
         template <typename U>
             requires Addable<T, U>
         Point<std::common_type_t<T, U>, dim> operator+(const Point<U, dim> &other) const
@@ -76,7 +101,12 @@ namespace Swarm
                                  { return coordinates[index] + other.coordinates[index]; });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise subtraction of the coordinates of two `Point`s.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise subtraction of the coordinates of two `Point`s.
+         * \param other The other `Point` to subtract.
+         * \return A new `Point` representing the elementwise difference.
+         * \see Subtractable
+         */
         template <typename U>
             requires Subtractable<T, U>
         Point<std::common_type_t<T, U>, dim> operator-(const Point<U, dim> &other) const
@@ -85,7 +115,12 @@ namespace Swarm
                                  { return coordinates[index] - other.coordinates[index]; });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise product of the coordinates by a scalar.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise multiplication of the coordinates by a scalar.
+         * \param other The scalar to multiply with.
+         * \return A new `Point` representing the elementwise product.
+         * \see Multipliable
+         */
         template <typename U>
             requires Multipliable<T, U>
         Point<std::common_type_t<T, U>, dim> operator*(const U &other) const
@@ -94,7 +129,12 @@ namespace Swarm
                                  { return coordinates[index] * other; });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise product of the coordinates of two `Point`s.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise multiplication of the coordinates of two `Point`s.
+         * \param other The other `Point` to multiply with.
+         * \return A new `Point` representing the elementwise product.
+         * \see Multipliable
+         */
         template <typename U>
             requires Multipliable<T, U>
         Point<std::common_type_t<T, U>, dim> operator*(const Point<U, dim> &other) const
@@ -103,7 +143,12 @@ namespace Swarm
                                  { return coordinates[index] * other.coordinates[index]; });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise division of the coordinates by a scalar.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise division of the coordinates by a scalar.
+         * \param other The scalar to divide by.
+         * \return A new `Point` representing the elementwise quotient.
+         * \see Divisible
+         */
         template <typename U>
             requires Divisible<T, U>
         Point<std::common_type_t<T, U>, dim> operator/(const U &other) const
@@ -112,7 +157,12 @@ namespace Swarm
                                  { return coordinates[index] / other; });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise division of the coordinates of two `Point`s.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise division of the coordinates of two `Point`s.
+         * \param other The other `Point` to divide by.
+         * \return A new `Point` representing the elementwise quotient.
+         * \see Divisible
+         */
         template <typename U>
             requires Divisible<T, U>
         Point<std::common_type_t<T, U>, dim> operator/(const Point<U, dim> &other) const
@@ -121,7 +171,13 @@ namespace Swarm
                                  { return coordinates[index] / other.coordinates[index]; });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise clamping between the coordinates of two other `Point`s defining an n-dimensional box.
+        /**
+         * \brief Returns a new `Point` whose coordinates are clamped between the corresponding coordinates of two other `Point`s.
+         * \param a The minimum `Point`.
+         * \param b The maximum `Point`.
+         * \return A new `Point` with clamped coordinates.
+         * \see TriComparable
+         */
         template <typename U, typename V>
             requires TriComparable<U, T, V>
         Point<T, dim> clamp(const Point<U, dim> &a, const Point<V, dim> &b) const
@@ -130,35 +186,55 @@ namespace Swarm
                                  { return std::max(a[index], std::min(coordinates[index], b[index])); });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::sin`.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::sin`.
+         * \return A new `Point` with the sine of each coordinate.
+         * \see std::sin
+         */
         Point<T, dim> sin() const
         {
             return Point<T, dim>([this](size_t index)
                                  { return std::sin(coordinates[index]); });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::asin`.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::asin`.
+         * \return A new `Point` with the arcsine of each coordinate.
+         * \see std::asin
+         */
         Point<T, dim> arcsin() const
         {
             return Point<T, dim>([this](size_t index)
                                  { return std::asin(coordinates[index]); });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::cos`.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::cos`.
+         * \return A new `Point` with the cosine of each coordinate.
+         * \see std::cos
+         */
         Point<T, dim> cos() const
         {
             return Point<T, dim>([this](size_t index)
                                  { return std::cos(coordinates[index]); });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::acos`.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::acos`.
+         * \return A new `Point` with the arccosine of each coordinate.
+         * \see std::acos
+         */
         Point<T, dim> arccos() const
         {
             return Point<T, dim>([this](size_t index)
                                  { return std::acos(coordinates[index]); });
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise exponentiation to a positive integer.
+        /**
+         * \brief Returns a new `Point` whose coordinates are obtained through the elementwise exponentiation to a positive integer.
+         * \param exponent The exponent to raise each coordinate to.
+         * \return A new `Point` with the coordinates raised to the power of `exponent`.
+         */
         Point<T, dim> pow(int exponent) const
         {
             Point<T, dim> remainder(1);
@@ -178,7 +254,10 @@ namespace Swarm
             return accum * remainder;
         }
 
-        // Returns the 1-norm of the point.
+        /**
+         * \brief Returns the 1-norm of the point.
+         * \return The 1-norm value.
+         */
         T norm1() const
         {
             T sum = 0;
@@ -189,7 +268,10 @@ namespace Swarm
             return sum;
         }
 
-        // Returns the square 2-norm of the point.
+        /**
+         * \brief Returns the squared 2-norm of the point.
+         * \return The squared 2-norm value.
+         */
         T squareNorm2() const
         {
             T sum = 0;
@@ -200,38 +282,58 @@ namespace Swarm
             return sum;
         }
 
-        // Returns the 2-norm of the point.
+        /**
+         * \brief Returns the 2-norm of the point.
+         * \return The 2-norm value.
+         */
         T norm2() const
         {
             return std::sqrt(squareNorm2());
         }
 
-        // Returns a new `Point` whose coordinates are obtained through the elementwise application of `std::abs`.
+        /**
+         * \brief Returns a new `Point` whose coordinates are the absolute values of the original coordinates.
+         * \return A new `Point` with the absolute values of each coordinate.
+         * \see std::abs
+         */
         Point<T, dim> abs() const
         {
             return Point<T, dim>([this](size_t index)
                                  { return std::abs(coordinates[index]); });
         }
 
-        // Returns a copy of the coordinate at the selected index.
+        /**
+         * \brief Returns the coordinate at the selected index.
+         * \param index The index of the coordinate to retrieve.
+         * \return The value of the coordinate at the specified index.
+         */
         T operator[](size_t index) const
         {
             return coordinates[index];
         }
 
-        // Returns a reference to the coordinate at the selected index.
+        /**
+         * \brief Returns a reference to the coordinate at the selected index.
+         * \param index The index of the coordinate to retrieve.
+         * \return A reference to the value of the coordinate at the specified index.
+         */
         T &operator[](size_t index)
         {
             return coordinates[index];
         }
 
-        // Returns the number of dimensions.
+        /**
+         * \brief Returns the dimensionality of the point.
+         * \return The number of dimensions.
+         */
         constexpr int getDimensions() const
         {
             return dim;
         }
 
-        // Makes `Point` printable with the standard `<<` operator.
+        /**
+         * \brief Makes `Point` printable with the standard `<<` operator.
+         */
         friend std::ostream &operator<<(std::ostream &os, const Point<T, dim> &p)
         {
             os << "[ ";
@@ -244,6 +346,11 @@ namespace Swarm
             return os;
         }
 
+        /**
+         * \brief Assignment operator for `Point`.
+         * \param other The other `Point` to assign from.
+         * \return A reference to the assigned `Point`.
+         */
         Point<T, dim> &operator=(Point<T, dim> other)
         {
             for (size_t i = 0; i < dim; i++)
@@ -255,7 +362,9 @@ namespace Swarm
         }
 
     private:
-        // Stores the value of each coordinate.
+        /**
+         * \brief The array storing the coordinates of the point.
+         */
         T coordinates[dim];
     };
 };
--- src/utils.hpp
+++ src/utils.hpp
@@ -3,28 +3,57 @@
 
 namespace Swarm
 {
+    /**
+     * \brief Type alias for iteration count.
+     */
     using IterationType = uint64_t;
 
+    /**
+     * \brief Concept requiring that the selected types supports addition.
+     * \tparam T The first type.
+     * \tparam U The second type.
+     */
     template <typename T, typename U>
     concept Addable = requires(T a, U b) {
         a + b;
     };
 
+    /**
+     * \brief Concept requiring that the selected types supports subtraction.
+     * \tparam T The first type.
+     * \tparam U The second type.
+     */
     template <typename T, typename U>
     concept Subtractable = requires(T a, U b) {
         a - b;
     };
 
+    /**
+     * \brief Concept requiring that the selected types supports multiplication.
+     * \tparam T The first type.
+     * \tparam U The second type.
+     */
     template <typename T, typename U>
     concept Multipliable = requires(T a, U b) {
         a * b;
     };
 
+    /**
+     * \brief Concept requiring that the selected types supports division.
+     * \tparam T The first type.
+     * \tparam U The second type.
+     */
     template <typename T, typename U>
     concept Divisible = requires(T a, U b) {
         a / b;
     };
 
+    /**
+     * \brief Concept requiring that the selected types supports triple comparison.
+     * \tparam T The first type.
+     * \tparam U The second type.
+     * \tparam V The third type.
+     */
     template <typename T, typename U, typename V>
     concept TriComparable = requires(T a, U b, V c) {
         a <= b <= c;
